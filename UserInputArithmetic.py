07a2715245371334e24d07e3b0fbb34e71ac172f
Bhaskar Gaikwad <gaikwadbhaskar188@gmail.com>
2025-04-14 20:45:16 +0530

    Create UserInputArithmetic.py

---
+++ UserInputArithmetic.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform arithmetic operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
