7830b2172f05ac32f21b1bb3a0caebadb720b439
Bhaskar Gaikwad <gaikwadbhaskar188@gmail.com>
2025-04-14 20:46:22 +0530

    Create Arithmatic.py

---
+++ Arithmatic.py
@@ -0,0 +1,9 @@
+# Define two numbers
+num1 = 10
+num2 = 5
+
+# Perform arithmetic operations
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
