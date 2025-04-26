43972dbca3d0b88a3d028a676a511d0c32113aa7
Bhaskar Gaikwad <gaikwadbhaskar188@gmail.com>
2025-04-27 01:38:55 +0530

    Update Arithmatic.py

--- Arithmatic.py
+++ Arithmatic.py
@@ -3,6 +3,8 @@ num1 = 10
 num2 = 5
 
 # Perform arithmetic operations
+print("Number1",num1)
+print("Number2",num2)
 print("Addition:", num1 + num2)
 print("Subtraction:", num1 - num2)
 print("Multiplication:", num1 * num2)
